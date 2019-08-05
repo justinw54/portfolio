ef7464b5ef8409b81bae5702e8a42883042bb39f
Justin White <justin.gj.white@gmail.com>
2019-08-05 08:20:50 -0400

    Server updates

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bd3f303936af7644aa8f";
+/******/ 	var hotCurrentHash = "c3054c8244b9187708f6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
